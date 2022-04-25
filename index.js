0b035034e4aa3c2011343e38346b0b1e5f5042f1
Salman <ssohrab375@gmail.com>
2022-04-25 14:21:45 +0530

    Added styling to my project.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ function calculateAmount(){
     let bankBalance1= document.getElementById('cx1').value;
     let bankBalance2= document.getElementById('cx2').value;
     let bankBalance3= document.getElementById('cx3').value;
+    let bankBalance4= document.getElementById('cx4').value;
+    let bankBalance5= document.getElementById('cx5').value;
 
-    let bankBalances= [bankBalance1, bankBalance2, bankBalance3];
+    let bankBalances= [bankBalance1, bankBalance2, bankBalance3,bankBalance4,bankBalance5];
 
     for(let i=0; i<bankBalances.length; i++){
         bankBalances[i]= Number(bankBalances[i]);
@@ -22,19 +24,29 @@ function calculateAmount(){
     let updatedBankBalance1= document.getElementById('updated-Bank-Balance1');
     let updatedBankBalance2= document.getElementById('updated-Bank-Balance2');
     let updatedBankBalance3= document.getElementById('updated-Bank-Balance3');
+    let updatedBankBalance4= document.getElementById('updated-Bank-Balance4');
+    let updatedBankBalance5= document.getElementById('updated-Bank-Balance5');
 
 
     updatedBankBalance1.value = bankBalances[0];
     updatedBankBalance2.value = bankBalances[1];
     updatedBankBalance3.value = bankBalances[2];
+    updatedBankBalance4.value = bankBalances[3];
+    updatedBankBalance5.value = bankBalances[4];
 }
 
 
-//OPTIMISED METHOD USING .getElementById:-
+//OPTIMISED METHOD USING .getElementByClassName:-
 
-    /*function calculateAmount(){
+   /* function calculateAmount(){
         let input_balances = document.getElementsByClassName('input-balances');
-        for(let i=0; i<input_balances.length; i++){
-            console.log(input_balances[i]);
+        //for(let i=0; i<input_balances.length; i++){
+        //    input_balances[i]= input_balances.value;
+        //}
+    
+
+        let display_final_balances= document.getElementsByClassName('display-final-balances');
+        for(let i=0; i< display_final_balances.length; i++){
+            display_final_balances[i].value= input_balances[i];
         }
     }*/
